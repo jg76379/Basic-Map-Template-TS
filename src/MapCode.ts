4d6fe9bd771b7fb6fe2c3e6b7fd80b23da6b78e3
James Gough <jgough7@gmail.com>
2020-02-06 11:48:42 -0500

    further reactor. app.ts is now the entry point src

--- src/MapCode.ts
+++ src/MapCode.ts
@@ -74,4 +74,4 @@ class MainMap {
   };
 }
 
-const map = new MainMap();
+export default MainMap;
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import MainMap from "./MapCode";
+
+
+function main(){
+    const mainMap = new MainMap();
+}
+
+main();
